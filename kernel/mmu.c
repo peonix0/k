3db33b602842d25a20fbc1b3c5ea27dca3ea9c8c
Ajit Singh <blfizzyy@gmail.com>
2025-09-29 14:20:23 +0530

    mmu: added gic and refactoring
    This commit add `generic interrupt controller` rough mapping, yet not
    correctly mapped just mapped in order to just make it work.
    
    Additionally remove the `stdint.h` by using own type def.
    
    Signed-off-by: Ajit Singh <blfizzyy@gmail.com>

--- kernel/mmu.c
+++ kernel/mmu.c
@@ -1,14 +1,14 @@
 #include "mmu.h"
 #include "uart.h"
 
-// __attribute__((aligned(4096))) static uint64_t L0[512];
-__attribute__((aligned(4096))) static uint64_t L1[512]; // 1GB Each
-//__attribute__((aligned(4096))) static uint64_t L2_kernel[512]; // 2 MB Each
-__attribute__((aligned(4096))) static uint64_t L2_uart[512];
+// __attribute__((aligned(4096))) static u64 L0[512];
+__attribute__((aligned(4096))) static u64 L1[512]; // 1GB Each
+//__attribute__((aligned(4096))) static u64 L2_kernel[512]; // 2 MB Each
+__attribute__((aligned(4096))) static u64 L2_t1[512];
 
 
 // --------------------- DUMPING- JUMPING-----------------------------------//
-// static void dump_sctlr(uint64_t sctlr) {
+// static void dump_sctlr(u64 sctlr) {
 //     uart_puts("SCTLR_EL1 = 0x%016llx  [M=%d C=%d I=%d A=%d SA=%d SA0=%d WXN=%d]\n",
 //            (unsigned long long)sctlr,
 //            (int)((sctlr>>0)&1),   // M: MMU enable
@@ -21,10 +21,10 @@ __attribute__((aligned(4096))) static uint64_t L2_uart[512];
 // }
 
 
-static inline uint64_t desc_block_2m(uint64_t pa, int attr_idx) {
-    uint64_t d = (pa & ~((1ULL<<21)-1))      // 2MiB aligned output address
+static inline u64 desc_block_2m(u64 pa, int attr_idx) {
+    u64 d = (pa & ~((1ULL<<21)-1))      // 2MiB aligned output address
                | (1ULL<<10)                  // AF
-               | ((uint64_t)attr_idx << 2)   // AttrIndx[4:2]
+               | ((u64)attr_idx << 2)   // AttrIndx[4:2]
                | (1 ? (1ULL<<54) : 0)       // UXN
                | (0 ? (1ULL<<53) : 0)       // PXN
                | (1ULL<<0);                  // VALID, TYPE=BLOCK
@@ -33,10 +33,10 @@ static inline uint64_t desc_block_2m(uint64_t pa, int attr_idx) {
     return d;
 }
 
-static inline uint64_t desc_block_1g(uint64_t pa_page, int attr_idx) {
-    uint64_t d = (pa_page & ~((1ULL<<30)-1))
+static inline u64 desc_block_1g(u64 pa_page, int attr_idx) {
+    u64 d = (pa_page & ~((1ULL<<30)-1))
                 | (1ULL<<10)
-                | ((uint64_t)attr_idx << 2) // AtrrIndx[4:2]
+                | ((u64)attr_idx << 2) // AtrrIndx[4:2]
                 | (1 ? (1ULL<<54) : 0) // UXN
                 | (0 ? (1ULL<<53) : 0) // PXN
                 | (1ULL << 0);  // VALID, TYPE=Table Entry
@@ -45,10 +45,10 @@ static inline uint64_t desc_block_1g(uint64_t pa_page, int attr_idx) {
     return d;
 }
 
-static inline uint64_t desc_page_4k(uint64_t pa_page, int attr_idx) {
-    uint64_t e = (pa_page & ~((1ULL<<12)-1))
+static inline u64 desc_page_4k(u64 pa_page, int attr_idx) {
+    u64 e = (pa_page & ~((1ULL<<12)-1))
                 | (1ULL<<10)
-                | ((uint64_t)attr_idx << 2) // AtrrIndx[4:2]
+                | ((u64)attr_idx << 2) // AtrrIndx[4:2]
                 | (1 ? (1ULL<<54) : 0) // UXN
                 | ( 0 ? (1ULL<<53) : 0) // PXN
                 | (3ULL << 0);  // VALID, TYPE=Table Entry
@@ -58,17 +58,17 @@ static inline uint64_t desc_page_4k(uint64_t pa_page, int attr_idx) {
 }
 
 // point to a 4KB page
-static inline uint64_t desc_table(uint64_t pa) {
+static inline u64 desc_table(u64 pa) {
     return (pa & ~0xFFFULL) | (1ULL<<1) | 1ULL; // VALID, TYPE=TABLE
 }
 
-static inline uint64_t tcr_value(void){
-    const uint64_t T0SZ  = 32ULL;   // 32-bit VA
-    const uint64_t TG0   = 0ULL;    // 4KB
-    const uint64_t SH0   = 3ULL;    // Inner
-    const uint64_t ORGN0 = 1ULL<<10 | 1ULL<<11; // WB WA
-    const uint64_t IRGN0 = 1ULL<<8  | 1ULL<<9;  // WB WA
-    const uint64_t IPS   = 0b010ULL<<32;        // 48-bit PA
+static inline u64 tcr_value(void){
+    const u64 T0SZ  = 32ULL;   // 32-bit VA
+    const u64 TG0   = 0ULL;    // 4KB
+    const u64 SH0   = 3ULL;    // Inner
+    const u64 ORGN0 = 1ULL<<10 | 1ULL<<11; // WB WA
+    const u64 IRGN0 = 1ULL<<8  | 1ULL<<9;  // WB WA
+    const u64 IPS   = 0b010ULL<<32;        // 48-bit PA
     return (T0SZ) | (TG0<<14) | (SH0<<12) | (IRGN0) | (ORGN0) | IPS;
 }
 
@@ -76,23 +76,32 @@ extern char __text_start[]; // pick your kernel base symbol
 void mmu_enable(void){
     // map kernel @ 0x40200000 as Normal WB
 
-    uint64_t kva = 0x40200000ULL;
+    u64 kva = 0x40200000ULL;
     L1[L1_IDX(kva)] = desc_block_1g(kva, 0x1);
-    //L0[L0_IDX(kva)] = desc_table((uint64_t) L1_kernel);
+    //L0[L0_IDX(kva)] = desc_table((u64) L1_kernel);
 
     // Map QEMU PL011 or RK UART as Device (AttrIdx0)
 #ifdef PLATFORM_qemu
-    uint64_t uva = 0x09000000ULL;
+    u64 uva = 0x09000000ULL;
 #else
-    uint64_t uva = 0xFEB50000ULL;
+    u64 uva = 0xFEB50000ULL;
 #endif
-    uint64_t l2_uart_entry = desc_block_2m(uva, 0x0);
-    L2_uart[L2_IDX(uva)] = l2_uart_entry;
-    L1[L1_IDX(uva)] = desc_table((uint64_t) L2_uart);
+    u64 l2_uart_entry = desc_block_2m(uva, 0x0);
+    L2_t1[L2_IDX(uva)] = l2_uart_entry;
 
-    uart_puts("mmu: configuring....\n");
+    // map GIC
+    u64 gic_dva = 0x08000000;  //64 KB
+    // u64 gic_rdva = 0x080A0000; // 2MB, I only care about single frame (128KB)
+
+    L2_t1[L2_IDX(gic_dva)] = desc_block_2m(gic_dva, 0x0);
+
+    // rdva region already mapped by above block entry (atleast one frame)
+    //L2_t1[L2_IDX(gic_rdva)] = desc_block_2m(gic_rdva, 0x0);
+
+    L1[L1_IDX(uva)] = desc_table((u64) L2_t1);
+
     // 2) Program MAIR/TCR/TTBR0
-    uint64_t mair = (MAIR_ATTR_DEVnGnRE << 0) | (MAIR_ATTR_NORMALWB << 8);
+    u64 mair = (MAIR_ATTR_DEVnGnRE << 0) | (MAIR_ATTR_NORMALWB << 8);
     __asm__ volatile(
         "msr mair_el1, %0\n"
         "msr tcr_el1,  %1\n"
@@ -106,7 +115,7 @@ void mmu_enable(void){
         : "memory");
 
     // 3) Enable MMU + caches (M|C|I)
-    uint64_t sctlr;
+    u64 sctlr;
     __asm__ volatile("mrs %0, sctlr_el1" : "=r"(sctlr));
     uart_putb("sctlr_el1: ", sctlr);
 
